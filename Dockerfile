887f67bdf07aeb15f0b8abc28493916d8a98d996
Maksym Pakula <gargantadoto@gmail.com>
2026-03-05 22:50:13 +0200

    Solution for current Paris weather

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.12.13-alpine3.23
+LABEL maintainer="strawhato"
+
+ENV PYTHOUNBUFFERED=1
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+URL = "https://api.weatherapi.com/v1/current.json"
+FILTERING = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    result = requests.get(URL, params={"key": API_KEY, "q": FILTERING})
+    print("Performing request to Weather API for city Paris...")
+    print(
+        f"{result.json()["location"]["name"]}/"
+        f"{result.json()["location"]["country"]} "
+        f"{result.json()["location"]["localtime"]}"
+        f" Weather {result.json()["current"]["temp_c"]} "
+        f"Celsius"
+    )
 
 
 if __name__ == "__main__":
